ec76fb014138ee680169f8922d410082b1d834f3
junsantilla <junsantilla@live.com>
2023-07-17 10:18:36 +0200

    Fixed an issue with the retype function that now includes the first letter

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@ All notable changes to the "code-typewriter-effect" extension will be documented
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
-## [Unreleased]
+## [0.1.3]
 
-- Initial release
+### Fixed
+
+- Fixed an issue with the "retype" function that now includes the first letter.
+
--- extension.js
+++ extension.js
@@ -5,71 +5,56 @@ const vscode = require("vscode");
  */
 
 function activate(context) {
-	console.log(
-		'Congratulations, your extension "code-typewriter-effect" is now active!'
-	);
+	console.log('Congratulations, your extension "code-typewriter-effect" is now active!');
 
-	let disposable = vscode.commands.registerCommand(
-		"code-typewriter-effect.retype",
-		function () {
-			// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand("code-typewriter-effect.retype", function () {
 
-			let editor = vscode.window.activeTextEditor;
-			if (!editor) {
-				return;
-			}
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
 
-			let currentPosition = 0;
-			let text = editor.document.getText();
+		let currentPosition = 0;
+		let text = editor.document.getText();
 
-			editor.edit((editBuilder) => {
-				editBuilder.delete(
-					new vscode.Range(0, 0, editor.document.lineCount, 0)
-				);
-			});
+		editor.edit((editBuilder) => {
+			editBuilder.delete(new vscode.Range(0, 0, editor.document.lineCount, 0));
+		}).then(() => {
+			type();
+		});
 
-			function type() {
-				let line = text.split("\n")[currentPosition];
-				let i = 0;
+		function type() {
+			let line = text.split("\n")[currentPosition];
+			let i = 0;
 
-				function insertNextCharacter() {
-					if (i < line.length) {
-						editor
-							.edit((editBuilder) => {
-								editBuilder.insert(
-									new vscode.Position(currentPosition, i),
-									line.charAt(i)
-								);
-							})
-							.then(() => {
-								i++;
-								setTimeout(insertNextCharacter, 5);
-							});
-					} else {
-						currentPosition++;
-						if (currentPosition < text.split("\n").length) {
-							setTimeout(type, 0);
-						}
+			function insertNextCharacter() {
+				if (i <= line.length) {
+					editor.edit((editBuilder) => {
+						editBuilder.insert(new vscode.Position(currentPosition, i), line.charAt(i));
+					}).then(() => {
+						i++;
+						setTimeout(insertNextCharacter, 5);
+					});
+				} else {
+					currentPosition++;
+					if (currentPosition < text.split("\n").length) {
+						line = text.split("\n")[currentPosition];
+						i = 0;
+						setTimeout(insertNextCharacter, 0);
 					}
 				}
-
-				insertNextCharacter();
 			}
 
-			// Display a message box to the user
-			// vscode.window.showInformationMessage("Retyping your code!");
-
-			// Start the typewriter animation
-			type();
+			insertNextCharacter();
 		}
-	);
+	});
 
 	context.subscriptions.push(disposable);
 }
 
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
-	deactivate,
+	deactivate
 };
